d577864adea79d4f4077c5c82d3812cd79003705
GorillaCode <benmans436@gmail.com>
2026-05-07 23:11:52 +0100

    feat: Implement basic PWA service worker
    Adds a service worker to cache application assets for offline access and faster loading. This moves towards making the JUSTBZ application a Progressive Web App.

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'justbz-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/src/main.tsx',
+  '/favicon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
